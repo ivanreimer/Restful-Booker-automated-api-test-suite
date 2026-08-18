5306aff12f078a74262ca28337386e2860a14cc5
ivanreimer <ivanreimer1@gmail.com>
2026-08-18 10:23:54 -0300

    booking.types creation

---
+++ src/types/booking.types.ts
@@ -0,0 +1,13 @@
+export type BookingDates = {
+  checkin: string;
+  checkout: string;
+};
+
+export type Booking = {
+  firstname: string;
+  lastname: string;
+  totalprice: number;
+  depositpaid: boolean;
+  bookingdates: BookingDates;
+  additionalneeds: string;
+};
