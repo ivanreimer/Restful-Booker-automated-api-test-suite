7bb6d1abfdd1b1ff3d032946360db1106ca21514
ivanreimer <ivanreimer1@gmail.com>
2026-08-20 19:43:16 -0300

    Data abstraction and doing more validations

--- tests/specs/auth.spec.ts
+++ tests/specs/auth.spec.ts
@@ -1,6 +1,15 @@
 import { authTest , expect } from "../fixtures/api.fixture";
+import { getRequiredEnv } from "../helpers/env.helper";
+
 
 authTest('Success login', async ({ authApi }) => {
-    const response = await authApi.login('admin', 'password123');
+    const username = getRequiredEnv('ADMIN_USERNAME');
+    const password = getRequiredEnv('ADMIN_PASSWORD');  
+    const response = await authApi.login(username, password);
     expect(response.status()).toBe(200);
+    const body = await response.json();
+
+    expect(body).toHaveProperty('token');
+    expect(typeof body.token).toBe('string');
+    expect(body.token.length).toBeGreaterThan(0);
 });
